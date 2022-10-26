e207db08f443737af21455e80005c3fd5e227f50
17893 <1789319416@qq.com>
2022-10-27 00:55:19 +0800

    修改——目标检测

--- app/src/main/java/com/example/news/ui/photograph/RecognizeActivity.java
+++ app/src/main/java/com/example/news/ui/photograph/RecognizeActivity.java
@@ -16,13 +16,19 @@ package com.example.news.ui.photograph;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
 import androidx.core.content.ContextCompat;
 import androidx.core.app.ActivityCompat;
 //import android.support.v4.app.ActivityCompat;
 //import android.support.v4.content.ContextCompat;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.BackgroundColorSpan;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -30,9 +36,24 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.news.R;
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechEvent;
+import com.iflytek.cloud.SpeechSynthesizer;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.SynthesizerListener;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class RecognizeActivity extends Activity implements SurfaceHolder.Callback
 {
@@ -45,8 +66,35 @@ public class RecognizeActivity extends Activity implements SurfaceHolder.Callbac
     private Spinner spinnerCPUGPU;
     private int current_model = 0;
     private int current_cpugpu = 0;
-
+    boolean isexit=false;
+    boolean ispause=false;
     private SurfaceView cameraView;
+    private String returnTag="";
+    //xunfei
+    private SpeechSynthesizer mTts;
+    private static String TAG = RecognizeActivity.class.getSimpleName();
+    // 默认发音人
+    private String voicer = "xiaoyan";
+
+    private String[] mCloudVoicersEntries;
+    private String[] mCloudVoicersValue;
+    private String texts = "";
+
+    // 缓冲进度
+    private int mPercentForBuffering = 0;
+    // 播放进度
+    private int mPercentForPlaying = 0;
+
+    // 云端/本地单选按钮
+    private RadioGroup mRadioGroup;
+    // 引擎类型
+    private String mEngineType = SpeechConstant.TYPE_CLOUD;
+
+    private Toast mToast;
+    private SharedPreferences mSharedPreferences;
+    private File pcmFile;
+
+
 
     /** Called when the activity is first created. */
     @Override
@@ -57,6 +105,8 @@ public class RecognizeActivity extends Activity implements SurfaceHolder.Callbac
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        SpeechUtility.createUtility(this, SpeechConstant.APPID +"=cf3e02db");
+        mTts = SpeechSynthesizer.createSynthesizer(this, mTtsInitListener);
         cameraView = (SurfaceView) findViewById(R.id.cameraview);
 
         cameraView.getHolder().setFormat(PixelFormat.RGBA_8888);
@@ -114,6 +164,39 @@ public class RecognizeActivity extends Activity implements SurfaceHolder.Callbac
         });
 
         reload();
+        new Thread(new Runnable() {
+
+            public void run() {
+
+                while (true) {
+
+                    if(isexit){
+                        break;
+                    }
+                    if(!ispause){
+                        texts = "";
+                        int flag = 0;
+                        returnTag = "";
+                        try {
+                            Thread.sleep(2 * 1000);
+                            returnTag = nanodetncnn.getTag();
+                            if (returnTag != "") {
+                                if (returnTag.indexOf("person") != -1) {
+                                    texts += "前方有人";
+                                    onPlay();
+                                    Log.v("Tag", returnTag);
+                                }
+
+                            }
+
+
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
     }
 
     private void reload()
@@ -145,7 +228,8 @@ public class RecognizeActivity extends Activity implements SurfaceHolder.Callbac
     public void onResume()
     {
         super.onResume();
-
+        ispause=false;
+        Log.v("test", String.valueOf(ispause));
         if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED)
         {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, REQUEST_CAMERA);
@@ -158,7 +242,192 @@ public class RecognizeActivity extends Activity implements SurfaceHolder.Callbac
     public void onPause()
     {
         super.onPause();
-
+        ispause=true;
+        //Log.v("test", String.valueOf(ispause));
         nanodetncnn.closeCamera();
+    }
+
+    @Override
+
+    protected void onDestroy() {
+
+        super.onDestroy();
+       //Log.v("destroy","exit" );
+        isexit = true;
+
+    }
+
+
+    public void onPlay() {
+        if (null == mTts) {
+            return;
+        }
+
+
+            // 开始合成
+            // 收到onCompleted 回调时，合成结束、生成合成音频
+            // 合成的音频格式：只支持pcm格式
+
+            pcmFile = new File(getExternalCacheDir().getAbsolutePath(), "tts_pcmFile.pcm");
+            pcmFile.delete();
+
+            // 设置参数
+            setParam();
+            // 合成并播放
+            int code = mTts.startSpeaking(texts, mTtsListener);
+//			/**
+//			 * 只保存音频不进行播放接口,调用此接口请注释startSpeaking接口
+//			 * text:要合成的文本，uri:需要保存的音频全路径，listener:回调接口
+//			*/
+//                String path = getExternalFilesDir("msc").getAbsolutePath() + "/tts.pcm";
+//                //  synthesizeToUri 只保存音频不进行播放
+//                int code = mTts.synthesizeToUri(texts, path, mTtsListener);
+
+            if (code != ErrorCode.SUCCESS) {
+                showTip("语音合成失败,错误码: " + code + ",请点击网址https://www.xfyun.cn/document/error-code查询解决方案");
+            }
+
+            // 取消合成
+
+
+    }
+
+    private InitListener mTtsInitListener = new InitListener() {
+        @Override
+        public void onInit(int code) {
+
+            if (code != ErrorCode.SUCCESS) {
+                Log.v("s","fail");
+            } else {
+                // 初始化成功，之后可以调用startSpeaking方法
+                // 注：有的开发者在onCreate方法中创建完合成对象之后马上就调用startSpeaking进行合成，
+                // 正确的做法是将onCreate中的startSpeaking调用移至这里
+            }
+        }
+    };
+
+    private SynthesizerListener mTtsListener = new SynthesizerListener() {
+
+        @Override
+        public void onSpeakBegin() {
+            showTip("开始播放");
+        }
+
+        @Override
+        public void onSpeakPaused() {
+            showTip("暂停播放");
+        }
+
+        @Override
+        public void onSpeakResumed() {
+            showTip("继续播放");
+        }
+
+        @Override
+        public void onBufferProgress(int percent, int beginPos, int endPos,
+                                     String info) {
+            // 合成进度
+            Log.e("MscSpeechLog_", "percent =" + percent);
+            mPercentForBuffering = percent;
+            //showTip(String.format(getString(R.string.tts_toast_format),mPercentForBuffering, mPercentForPlaying));
+        }
+
+        @Override
+        public void onSpeakProgress(int percent, int beginPos, int endPos) {
+            // 播放进度
+            Log.e("MscSpeechLog_", "percent =" + percent);
+            mPercentForPlaying = percent;
+            //showTip(String.format(getString(R.string.tts_toast_format),mPercentForBuffering, mPercentForPlaying));
+
+            SpannableStringBuilder style = new SpannableStringBuilder(texts);
+            Log.e(TAG, "beginPos = " + beginPos + "  endPos = " + endPos);
+            style.setSpan(new BackgroundColorSpan(Color.RED), beginPos, endPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            //((EditText) findViewById(R.id.tts_text)).setText(style);
+        }
+
+        @Override
+        public void onCompleted(SpeechError error) {
+            showTip("播放完成");
+            if (error != null) {
+                showTip(error.getPlainDescription(true));
+                return;
+            }
+        }
+
+        @Override
+        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
+            //	 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
+            //	 若使用本地能力，会话id为null
+            if (SpeechEvent.EVENT_SESSION_ID == eventType) {
+                String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
+                Log.d(TAG, "session id =" + sid);
+            }
+            // 当设置 SpeechConstant.TTS_DATA_NOTIFY 为1时，抛出buf数据
+            if (SpeechEvent.EVENT_TTS_BUFFER == eventType) {
+                byte[] buf = obj.getByteArray(SpeechEvent.KEY_EVENT_TTS_BUFFER);
+                Log.e(TAG, "EVENT_TTS_BUFFER = " + buf.length);
+                // 保存文件
+                appendFile(pcmFile, buf);
+            }
+
+        }
+    };
+
+    private void showTip(final String str) {
+        runOnUiThread(() -> {
+            if (mToast != null) {
+                mToast.cancel();
+            }
+            mToast = Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT);
+            mToast.show();
+        });
+    }
+    private void setParam() {
+        // 清空参数
+        mTts.setParameter(SpeechConstant.PARAMS, null);
+        // 根据合成引擎设置相应参数
+        if (mEngineType.equals(SpeechConstant.TYPE_CLOUD)) {
+            mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD);
+            // 支持实时音频返回，仅在 synthesizeToUri 条件下支持
+            mTts.setParameter(SpeechConstant.TTS_DATA_NOTIFY, "1");
+            //	mTts.setParameter(SpeechConstant.TTS_BUFFER_TIME,"1");
+
+            // 设置在线合成发音人
+            mTts.setParameter(SpeechConstant.VOICE_NAME, voicer);
+            //设置合成语速
+            mTts.setParameter(SpeechConstant.SPEED, "50");
+            //设置合成音调
+            mTts.setParameter(SpeechConstant.PITCH, "50");
+            //设置合成音量
+            mTts.setParameter(SpeechConstant.VOLUME, "50");
+        } else {
+            mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_LOCAL);
+            mTts.setParameter(SpeechConstant.VOICE_NAME, "");
+
+        }
+
+        //设置播放器音频流类型
+        mTts.setParameter(SpeechConstant.STREAM_TYPE, "3");
+        // 设置播放合成音频打断音乐播放，默认为true
+        mTts.setParameter(SpeechConstant.KEY_REQUEST_FOCUS, "false");
+
+        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+        mTts.setParameter(SpeechConstant.AUDIO_FORMAT, "pcm");
+        mTts.setParameter(SpeechConstant.TTS_AUDIO_PATH,
+                getExternalFilesDir("msc").getAbsolutePath() + "/tts.pcm");
+    }
+
+    private void appendFile(File file, byte[] buffer) {
+        try {
+            if (!file.exists()) {
+                boolean b = file.createNewFile();
+            }
+            RandomAccessFile randomFile = new RandomAccessFile(file, "rw");
+            randomFile.seek(file.length());
+            randomFile.write(buffer);
+            randomFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
